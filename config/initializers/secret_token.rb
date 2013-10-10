c0698dcaf4631b5a6139ff2b2e342287b61c1c25
Prashant Mishra <prashantm@QE-IND-WKS002-VM.(none)>
2013-10-10 16:42:01 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Forum::Application.config.secret_token = '3a78dde4511d72ea978bf92383637ddb91aa9f300f9bf3763944d5bde0cea8252bf8d719f480cc46defd16261b9a882a3ef8526b823bec5402bc5097ba4863e6'
